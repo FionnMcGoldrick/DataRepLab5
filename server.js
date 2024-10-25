fd74fecb8eb4f0181913607f6175c0ce3539cca7
Fionn McGoldrick <G00422349@atu.ie>
2024-10-25 11:25:14 +0000

    added comments. finished

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express'); // Importing the express module
+const app = express(); // Creating an instance of an Express application
+const port = 3000; // Setting the port for the server
 
+// Route for the root URL
 app.get('/', (req, res) => {
-    res.send('Welcome to Data Respresentation & Querying');
+    res.send('Welcome to Data Representation & Querying'); // Responding with a welcome message
 });
 
+// Starting the server and logging the URL to the console
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-//Error Handling
+// Error Handling
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+    console.error(err.stack); // Logging the error stack to the console
+    res.status(500).send('Something went wrong!'); // Sending a 500 status code with an error message
 });
 
-//Route with URL Parameters
-/*app.get('/hello/:name/:surname', (req, res) => {
+// Route with URL Parameters
+/* 
+app.get('/hello/:name/:surname', (req, res) => {
     res.send('Hello ' + req.params.name + " " + req.params.surname);
-}); */
+}); 
+*/
 
-//get API Movies
+// Get API Movies
 app.get('/api/movies', (req, res) => {
     const movies = [
         {
@@ -46,34 +50,34 @@ app.get('/api/movies', (req, res) => {
             "Poster": "https://example.com/poster3.jpg"
         }
     ];
-    //Status code 
-    res.status(200).json({ myMovies: movies });
+    // Status code 
+    res.status(200).json({ myMovies: movies }); // Responding with a 200 status and sending the movies as JSON
 });
 
-//Path for HTML index page
-const path = require('path');
+// Path for HTML index page
+const path = require('path'); // Importing the path module
 
 app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html')); // Sending the index.html file as a response
 });
 
-//Server static assets
-app.use(express.static('public'));
+// Server static assets
+app.use(express.static('public')); // Serving static files from the 'public' directory
 
-//Get Request
+// Get Request
 app.get('/name', (req, res) => {
-    const firstname = req.query.firstname;
-    const lastname = req.query.lastname;
-    res.send(`Hello ${firstname} ${lastname}`);
+    const firstname = req.query.firstname; // Getting the first name from query parameters
+    const lastname = req.query.lastname; // Getting the last name from query parameters
+    res.send(`Hello ${firstname} ${lastname}`); // Responding with a greeting message
 });
 
-//Body Parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+// Body Parser
+const bodyParser = require('body-parser'); // Importing the body-parser module
+app.use(bodyParser.urlencoded({ extended: true })); // Configuring body-parser to parse URL-encoded data
 
-//Handle Post form 
+// Handle Post form 
 app.post('/name', (req, res) => {
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    res.send(`Hello ${firstname} ${lastname}`);
+    const firstname = req.body.firstname; // Getting the first name from the request body
+    const lastname = req.body.lastname; // Getting the last name from the request body
+    res.send(`Hello ${firstname} ${lastname}`); // Responding with a greeting message
 });
